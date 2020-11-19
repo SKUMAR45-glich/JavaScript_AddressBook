125ccf05e5c4cc79d745282bb2619c72ac7a9d99
Saurabh <saurabh.nit20@gmail.com>
2020-11-20 01:16:43 +0530

    [Saurabh].Creation of Contact

---
+++ ContactDetails.js
@@ -0,0 +1,21 @@
+﻿class ContactDetails {
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zip;
+    phNumber;
+    email;
+
+    ContactDetails(firstName, lastName, address, city, state, zip, phNumber, email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phNumber = phNumber;
+        this.email = email;
+    }
+}
